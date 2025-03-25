5a49e743637ee6805eb4b55a0a1892fbf912b778
Youssouf ⚜️ <71589435+Yusufibin@users.noreply.github.com>
2025-03-25 01:14:25 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Le maître Traore', // Your name
-  ownerNumber: '24176240183', //Your number
+  ownerNumber: '24177224940', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
